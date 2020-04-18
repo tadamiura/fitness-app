2550d7a2a3d05d616244931d0660c0ba39ef2cdc
tadamiura <tadamiura@gmail.com>
2020-04-18 15:08:00 +0200

    Create RandomExercices & strenghtexercices component

---
+++ src/components/StrenghtProgram.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+class StrenghtProgram extends React.Component {
+ constructor(props){
+     super(props);
+     this.state = {
+         showUpperOnly : false,
+         exercicesList: [
+             {
+                 exerciceName : 'pompes',
+                 partOfBody : 'upper',
+             },
+             {
+                 exerciceName : 'squat',
+                 partOfBody : "lower",
+             }
+         ]
+     }
+ }
+
+ handleShowStrenghtExercice = () => {
+     this.setState({showUpperOnly : !this.state.showUpperOnly
+    });
+ }
+
+ render() {
+     return (
+         <div>
+            <button type='button' className='callExercice' onClick={this.handleShowStrenghtExercice}>
+             Exercice Upper Body: {this.state.showUpperOnly ? 'ON' : 'OFF'}
+        </button>
+         {this.state.exercicesList.exerciceName}
+         </div>
+
+     )
+ }
+}
+
+
+export default StrenghtProgram
