6fee9a713da755af30d6fa234a7967296b0853ef
tadamiura <tadamiura@gmail.com>
2020-04-26 13:13:06 +0200

    add Abs to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import AbsExercices from './components/AbsExercices';
 import BicepsExercices from './components/BicepsExercices';
 import ChestExercices from './components/ChestExercices';
+import LegsExercices from './components/LegsExercices';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <BicepsExercices />
-    <ChestExercices />
+      <LegsExercices />
+      <AbsExercices />
+      <BicepsExercices />
+      <ChestExercices />
     </div>
   );
 }
