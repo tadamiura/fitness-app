23b442328922e5a5c6e5cc96fc1de9c94e60ae68
tadamiura <tadamiura@gmail.com>
2020-05-03 10:53:58 +0200

    Create a StrenghtProgram component to use all components who call API and App.js display it

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import AbsExercices from './components/AbsExercices';
-import BicepsExercices from './components/BicepsExercices';
-import ChestExercices from './components/ChestExercices';
-import LegsExercices from './components/LegsExercices';
+import StrenghtProgram from './components/StrenghtProgram';
+// import AbsExercices from './components/AbsExercices';
+// import BicepsExercices from './components/BicepsExercices';
+// import ChestExercices from './components/ChestExercices';
+// import LegsExercices from './components/LegsExercices';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <LegsExercices />
-      <AbsExercices />
-      <BicepsExercices />
-      <ChestExercices />
+      <StrenghtProgram />
     </div>
-  );
+  )
 }
 
 export default App;
