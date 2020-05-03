536d72a55dd77d9698b3e8701d3a48e612a0e20c
tadamiura <tadamiura@gmail.com>
2020-05-03 13:55:15 +0200

    Create a countdown component

---
+++ src/components/tabata/Countdown.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+class Countdown extends React.Component {
+    state = {
+        minutes: 4,
+        seconds: 0,
+    }
+
+    componentDidMount() {
+        this.myInterval = setInterval(() => {
+            const { seconds, minutes } = this.state
+
+            if (seconds > 0) {
+                this.setState(({ seconds }) => ({
+                    seconds: seconds - 1
+                }))
+            }
+            if (seconds === 0) {
+                if (minutes === 0) {
+                    clearInterval(this.myInterval)
+                } else {
+                    this.setState(({ minutes }) => ({
+                        minutes: minutes - 1,
+                        seconds: 59
+                    }))
+                }
+            } 
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.myInterval)
+    }
+
+    render() {
+        const { minutes, seconds } = this.state
+        return (
+            <div>
+                { minutes === 0 && seconds === 0
+                    ? <h1>Well done!</h1>
+                    : <h1>Time Remaining: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                }
+            </div>
+        )
+    }
+}
+
+export default Countdown
