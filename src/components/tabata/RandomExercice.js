8ddbce8e1169d519a9d8c0a4d41514bbbd934b6f
Miura <tadamiura@gmail.com>
2020-08-30 15:10:42 +0200

    changed to call API and hook instead of state and local array

--- src/components/tabata/RandomExercice.js
+++ src/components/tabata/RandomExercice.js
@@ -1,174 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import DisplayExercice from './DisplayExercice'
 
-// const tabataExercices = [
-//     {
-//         id: 0,
-//         name: 'push-up',
-//         part: 'upper',
-//         equipment: 'none'
-//     },
-//     {
-//         id: 1,
-//         name: 'squat',
-//         part: 'lower',
-//         equipment: 'none'
-//     },
-//     {
-//         id: 2,
-//         name: 'alternate lunge',
-//         part: 'lower',
-//         equipment: 'none'
-//     },
-//     {
-//         id: 3,
-//         name: 'sit-up',
-//         part: 'abs',
-//         equipment: 'none'
-//     },
-//     {
-//         id: 4,
-//         name: 'pull-up',
-//         part: 'upper',
-//         equipment: 'fix bar'
-//     },
-//     {
-//         id: 5,
-//         name: 'plank',
-//         part: 'abs',
-//         equipment: 'none'
-//     },
-//     {
-//         id: 6,
-//         name: 'mountain climber',
-//         part: 'abs',
-//         equipment: 'none'
-//     },
-//     {
-//         id: 7,
-//         name: 'swing',
-//         part: 'lower',
-//         equipment: 'dumbbell'
-//     },
-//     {
-//         id: 8,
-//         name: 'deadlift',
-//         part: 'lower',
-//         equipment: 'barbell'
-//     },
-//     {
-//         id: 9,
-//         name: 'hips thruster',
-//         part: 'lower',
-//         equipment: 'none'
-//     },
-//     {
-//         id: 10,
-//         name: 'bench',
-//         part: 'upper',
-//         equipment: 'barbell'
-//     },
-//     {
-//         id: 11,
-//         name: 'shoulder press',
-//         part: 'upper',
-//         equipment: 'barbell'
-//     },
-//     {
-//         id: 12,
-//         name: 'rowing',
-//         part: 'upper',
-//         equipment: 'barbell'
-//     },
-//     {
-//         id: 13,
-//         name: 'sumo squat',
-//         part: 'lower',
-//         equipment: 'none'
-//     },
-//     {
-//         id: 14,
-//         name: 'squat jump',
-//         part: 'lower',
-//         equipment: 'none'
-//     },
-//     {
-//         id: 15,
-//         name: 'alternate lunge jump',
-//         part: 'lower',
-//         equipment: 'none'
-//     },
-//     {
-//         id: 16,
-//         name: 'V-up',
-//         part: 'abs',
-//         equipment: 'none'
-//     },
-//     {
-//         id: 17,
-//         name: 'turkish get-up',
-//         part: 'abs',
-//         equipment: 'dumbbell'
-//     },
-//     {
-//         id: 18,
-//         name: 'burpees',
-//         part: 'abs',
-//         equipment: 'none'
-//     }
-// ]
+const RandomExercice = () => {
 
-// class RandomExercice extends React.Component {
-//     state = {
-//         name: '',
-//         part: '',
-//         equipment: ''
-//     }
+    const [exercices, setExercices] = useState(null)
 
-//     randomNumber = () => Math.floor(Math.random() * Math.floor(tabataExercices.length))
-
-//     getRandomTabataExercice = (number) => {
-//         this.setState({ name: tabataExercices[number].name, equipment: tabataExercices[number].equipment, id: tabataExercices[number].id })
-//     }
-
-//     componentDidMount() {
-//         this.getRandomTabataExercice(this.randomNumber())
-//     }
-
-//     render() {
-//         const { name, equipment, id } = this.state
-//         return (
-//             <div>
-//                 <DisplayExercice 
-//                 name={name} 
-//                 // equipment={equipment} 
-//                 id={id} />
-//             </div>
-//         )
-//     }
-// }
-
-const RandomExercice = (props) => {
-
-    const [randomExercice, setRandomExercice] = useState(null)
-
-    const getRandomExercice = () => {
-        // const url = 'http://localhost:8080/api/exercice'
-        axios.get(`http://localhost:8080/api/exercice`)
-        .then((res) => setRandomExercice(res.data))
+    const getExercices = () => {
+        const url = 'http://localhost:8080/api/exercice'
+        axios.get(url)
+        .then((res) => {
+            const random = Math.floor(Math.random() * Math.floor(res.data.length))
+            setExercices(res.data[random])
+            }
+        )
     }
 
-    useEffect(() => getRandomExercice(), [])
-
-    return randomExercice === null ?
+    useEffect(() => getExercices(), [])
+  
+    return exercices === null ?
     <p>Loading ...</p>
     :(
-        <div className='random-exercice'>
-            {randomExercice.map( exercice => 
-                <div key={exercice.id}>
-                    {exercice.name}
-                </div>)}
+        <div className='random-exercice' id={exercices.id}>
+            {exercices.name}
         </div>
     )
 
