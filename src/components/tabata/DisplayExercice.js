3ddcc08bf00c93ca45553f51e4c3c41aaca1e4e0
Miura <tadamiura@gmail.com>
2020-09-06 12:15:57 +0200

    created useState and useEffect to display media on click

--- src/components/tabata/DisplayExercice.js
+++ src/components/tabata/DisplayExercice.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const DisplayExercice = ({ exercices }) =>{
+const DisplayExercice = ({ id, exercices }) =>{
+
+    const [medias, setMedias] = useState(null)
+
+    const getMedias = () => {
+        const url = `http://localhost:8080/api/exercice/${id}/media`
+        axios.get(url)
+        .then((res) => {
+            setMedias(res.data[0])
+            || console.log(res.data[0])
+        }
+    )
+}
+    useEffect(() => getMedias(), [])
+
     return(
+        medias === null ?
+        <div>Loading ...</div> : 
         <div className='random-exercice' id={exercices.id}>
-        {exercices.name}
+        {medias.media_type === "video" ?        
+        <a target='blank' href={medias.url_name}>{exercices.name}</a>
+        : 
+        <p>{exercices.name}</p>
+        }
         </div>
     )
 }
