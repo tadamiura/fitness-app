807f41714cf5acaef8c6170f02b4187befca67fb
Miura <tadamiura@gmail.com>
2020-10-14 22:51:08 +0200

    added back office routes and menu burger

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import AddExercice from './components/backoffice/AddExercice';
+import BurgerMenu from './components/BurgerMenu';
 import Countdown from './components/tabata/Countdown';
 import Footer from './components/Footer'
 import Header from './components/Header';
-import TabataHome from './components/tabata/TabataHome';
+import Home from './components/backoffice/Home';
 import { Switch, Route } from 'react-router-dom'
+import TabataHome from './components/tabata/TabataHome';
+
 import './App.css';
 
 const App = () => {
 
     return (
       <div className="App">
-          <Header />
-            <Switch>
-            <Route exact path="/" component={TabataHome}/>
-            <Route path="/countdown" component={Countdown} />
-            </Switch>
-          <Footer />
+        <BurgerMenu />
+        <Header />
+          <Switch>
+          <Route exact path="/" component={TabataHome}/>
+          <Route path="/countdown" component={Countdown} />
+          <Route exact path="/back-office" component={Home} />
+          <Route path="/back-office/add" component={AddExercice} />
+          </Switch>
+        <Footer />
         </div>
       )
 }
