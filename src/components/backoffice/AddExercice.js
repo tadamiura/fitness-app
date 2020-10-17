0a6d6be8475149a5a2086b292acac2d9f502a8d7
Miura <tadamiura@gmail.com>
2020-10-17 18:52:50 +0200

    added media post

--- src/components/backoffice/AddExercice.js
+++ src/components/backoffice/AddExercice.js
@@ -1,15 +1,15 @@
-import Axios from 'axios';
 import React, { useState } from 'react'
 import axios from 'axios' 
 import '../../App.css'
 
 const AddExercice = () => {
     const [name, setName] = useState({
-        name: ''
+        name: '',
+        url_name: ''
     });
 
     const handleChange = (e) => {
-        setName({...name, name : e.target.value})
+        setName({ ...name, [e.target.name]: e.target.value })
     }
 
     const handleSubmit = (e) => {
@@ -17,7 +17,7 @@ const AddExercice = () => {
         e.preventDefault()
         axios
         .post(url, name)
-        .then((res) => res.data) 
+        .then((res) => res.data || console.log(res.data)) 
         .then((res) => alert(`${res}, l'exercice a bien été ajouté`))
         .catch((err) => alert(`erreur : ${err.response.data} `))
     }
@@ -27,8 +27,13 @@ const AddExercice = () => {
             <form className="add-exercice-form" onSubmit={handleSubmit}>
                 <h2>Ajouter un exercice</h2>
                 <div className="form-container">
+
                     <label htmlFor="name">Nom de l'exercice</label><br />
-                    <input className="form-input" type="text" id="name" name="name" placeholder="nouvel exercice" required onChange={handleChange}></input>
+                    <input className="form-input" type="text" id="name" name="name" placeholder="nouvel exercice" required onChange={handleChange}></input><br />
+
+                    <label htmlFor="url_name">Lien YOUTUBE de l'exercice</label><br />
+                    <input className="form-input" type="text" id="url_name" name="url_name" placeholder="lien youtube du nouveau exercice" onChange={handleChange}></input>
+
                     <input className="button-add" type="submit" value="Ajouter" />  
                 </div>
             </form>
