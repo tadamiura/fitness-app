790c2d4f3c7f2b1a42a421f8594344c440994d34
Miura <tadamiura@gmail.com>
2020-10-17 19:30:12 +0200

    styled form and button

--- src/components/backoffice/AddExercice.js
+++ src/components/backoffice/AddExercice.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios' 
 import '../../App.css'
+import './AddExercice.css'
 
 const AddExercice = () => {
     const [name, setName] = useState({
@@ -25,16 +26,16 @@ const AddExercice = () => {
     return (
         <div>
             <form className="add-exercice-form" onSubmit={handleSubmit}>
-                <h2>Ajouter un exercice</h2>
+                <h2 className="form-title">Ajouter un exercice</h2>
                 <div className="form-container">
 
                     <label htmlFor="name">Nom de l'exercice</label><br />
                     <input className="form-input" type="text" id="name" name="name" placeholder="nouvel exercice" required onChange={handleChange}></input><br />
 
                     <label htmlFor="url_name">Lien YOUTUBE de l'exercice</label><br />
-                    <input className="form-input" type="text" id="url_name" name="url_name" placeholder="lien youtube du nouveau exercice" onChange={handleChange}></input>
+                    <input className="form-input" type="text" id="url_name" name="url_name" placeholder="lien youtube du nouveau exercice" onChange={handleChange}></input><br />
 
-                    <input className="button-add" type="submit" value="Ajouter" />  
+                    <input className="form-button-add" type="submit" value="Ajouter" />  
                 </div>
             </form>
         </div>
