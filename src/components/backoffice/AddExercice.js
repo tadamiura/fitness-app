383effaa9d65f336ee39ad93beb448da28ab8774
Miura <tadamiura@gmail.com>
2020-11-07 12:50:25 +0100

    styled and fixed form of authentification

--- src/components/backoffice/AddExercice.js
+++ src/components/backoffice/AddExercice.js
@@ -18,7 +18,7 @@ const AddExercice = () => {
         e.preventDefault()
         axios
         .post(url, name)
-        .then((res) => res.data || console.log(res.data)) 
+        .then((res) => res.data) 
         .then((res) => alert(`${res}, l'exercice a bien été ajouté`))
         .catch((err) => alert(`erreur : ${err.response.data} `))
     }
--- src/components/tabata/Registration.js
+++ src/components/tabata/Registration.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import axios from 'axios'
 
 function Registration() {
     const { register, handleSubmit, errors, formState } = useForm({
         mode: "onblur"
     })
 
-    const onSubmit= data => {
-        console.log(data)
-    }
-
+    const onSubmit = data => {
+        axios.post('http://localhost:8080/api/register', data)
+        // .then(data)
+        .then((res) => res.data)
+        .then((res) => alert(`Félicitations, votre profil a bien étét enrengistré`))
+        .catch((err) => alert(`erreur : ${err.response.data} `))
+      }
+      
     const styles = {
         container: {
           width: "80%",
-          margin: "0 auto",
+          margin: "25px auto",
         },
         input: {
           width: "100%",
@@ -25,47 +30,64 @@ function Registration() {
     <div>
         <h2>Inscription</h2>
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input 
-            name="prenom" 
-            ref={register({
-                required: true,
-                maxLength: 80
-            })} 
-            placeholder="Prénom"
-            />
-            <span className="form-error-message">{errors.prenom && "Un prénom est requis"}</span>
-            <input 
-            name="nom" 
-            ref={register({
-                required: true,
-                maxLength: 80,
-                pattern: /^[A-Za-z]+$/i,
-            })}             
-            placeholder="Nom"
-            />
-            <span className="form-error-message">{errors.nom && "Un nom est requis"}</span>
-            <input 
-            name="email" 
-            ref={register({
-                required: true,
-                maxLength: 100,
-                pattern: /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/,
-            })}             
-            placeholder="Email" 
-            />
-            <span className="form-error-message">{errors.email && "Un email valide est requis"}</span>
-            <input 
-            name="password" 
-            ref={register({
-                required: true,
-                minLength: 6,
-            })} 
-            placeholder="Password" 
-            />
-            <span className="form-error-message">{errors.password && "Un mot de passe de 6 caractères minimium est requis"}</span>
-            <button type="submit" disabled={formState.isSubmitting}>
-                Valider
-            </button>
+            <div className="label-input-container">
+              <label >Prénom :</label>
+              <input 
+              name="prenom" 
+              ref={register({
+                  required: true,
+                  maxLength: 80
+              })} 
+              placeholder="Prénom"
+              /><br></br>
+              <span className="form-error-message">{errors.prenom && "Un prénom est requis"}</span>
+            </div>
+            <div className="label-input-container">
+              <label >Nom :</label>
+              <input 
+              name="nom" 
+              ref={register({
+                  required: true,
+                  maxLength: 80,
+              })}             
+              placeholder="Nom"
+              /><br></br>
+              <span className="form-error-message">{errors.nom && "Un nom est requis"}</span>
+            </div>
+            <div className="label-input-container">
+              <label >Votre adresse mail :</label>
+              <input 
+              name="email" 
+              ref={register({
+                  required: true,
+                  maxLength: 100,
+                  pattern: /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/,
+              })}             
+              placeholder="Email" 
+              /><br></br>
+              <span className="form-error-message">{errors.email && "Un email valide est requis"}</span>
+            </div>
+            <div className="label-input-container">
+              <label >Votre mot de passe : </label>
+              <p className="password-format-condition">Au moins 6 caractères requis</p>
+              <input 
+              name="password" 
+              ref={register({
+                  required: true,
+                  minLength: 6,
+              })} 
+              placeholder="Password" 
+              type="password"
+              
+              /><br></br>
+              <span className="form-error-message">{errors.password && "Un mot de passe de 6 caractères minimium est requis"}</span>
+
+            </div>
+            <div>
+              <button type="submit" disabled={formState.isSubmitting}>
+                  Valider
+              </button>
+            </div>
       </form>
     </div>
     )
