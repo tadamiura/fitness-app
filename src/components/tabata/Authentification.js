6afdca7f6f0ab848e7a1abf43681b1f371c1c4e8
Miura <tadamiura@gmail.com>
2020-11-11 11:25:19 +0100

    created an auth form

---
+++ src/components/tabata/Authentification.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
+
+function Authentification() {
+    const { register, handleSubmit, errors, formState } = useForm({
+        mode: "onblur"
+    })
+
+    const history = useHistory()
+
+    const onSubmit = data => { 
+        axios.post('http://localhost:8080/api/auth', data)
+        .then(res => res.headers['x-access-token'])
+        .then( data => { 
+          localStorage.clear()
+          localStorage.setItem('x-access-token', data)
+          history.push('/back-office/new-exercice')        
+        })
+        .then((res) => alert(`Vous êtes désormais connectés`))
+        .catch((err) => alert(`Identifiants incorrects`))
+      }
+
+    return(
+    <div>
+        <h2>Se connecter</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="label-input-container">
+              <label >Connectez-vous avec votre adresse mail :</label><br></br>
+              <input 
+              name="email" 
+              ref={register({
+                  required: true,
+                  maxLength: 100,
+                  pattern: /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/,
+              })}             
+              placeholder="Email" 
+              /><br></br>
+              <span className="form-error-message">{errors.email && "Un email valide est requis"}</span>
+            </div>
+            <div className="label-input-container">
+              <label >Votre mot de passe : </label>
+              <input 
+              name="password" 
+              ref={register({
+                  required: true,
+                  minLength: 6,
+              })} 
+              placeholder="Password" 
+              type="password"
+              
+              /><br></br>
+              <span className="form-error-message">{errors.password && "Un mot de passe de 6 caractères minimium est requis"}</span>
+
+            </div>
+            <div>
+              <button type="submit" disabled={formState.isSubmitting}>
+                  Valider
+              </button>
+            </div>
+      </form>
+    </div>
+    )
+}
+
+export default Authentification
