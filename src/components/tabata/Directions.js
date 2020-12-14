5a161c0d7af78098232ebe1fb5b9bd6507796ac3
Miura <tadamiura@gmail.com>
2020-12-14 18:21:24 +0100

    directions for tabata workout are in a own componant

---
+++ src/components/tabata/Directions.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Directions = () => {
+    return (
+        <div>
+             <h3>Les instructions :</h3>
+                <p>Pendant 4 minutes, alternez deux exercices pendant 20 secondes.</p>
+                <p>A la fin de chaque exercice, prenez une pause de 10 secondes</p>
+                <p>A la fin du circuit, si vous désirez poursuivre votre entraînement, récupérer 2 minutes avant de débuter le prochain circuit</p>
+        </div>
+    )
+}
+
+export default Directions   
