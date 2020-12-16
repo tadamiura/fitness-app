feab699da1ca5ad5fbfd63aa3220a808cf381d66
Miura <tadamiura@gmail.com>
2020-12-16 20:03:43 +0100

    renamed Home to List and added BackOfficeDashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import AboutMe from './components/AboutMe'
 import AddExercice from './components/backoffice/AddExercice'
 import Authentification from './components/tabata/Authentification'
-import AboutMe from './components/AboutMe'
 import AboutTabata from './components/AboutTabata'
+import BackOfficeDashboard from './components/backoffice/BackOfficeDashboard';
 import Countdown from './components/tabata/Countdown'
 import Dashboard from './components/Dashboard'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import Home from './components/backoffice/Home'
+import List from './components/backoffice/List'
 import TabataHome from './components/tabata/TabataHome'
 import TabataWorkout from './components/tabata/Workout'
 import Registration from './components/tabata/Registration'
@@ -29,7 +30,8 @@ const App = () => {
           <Route path="/about-tabata" component={AboutTabata} />
           <Route path="/registration" component={Registration}/>
           <Route path="/auth/login" component={Authentification}/>
-          <Route exact path="/back-office" component={Home} />
+          <Route exact path="/back-office" component={BackOfficeDashboard} />
+          <Route path="/back-office/exercices" component={List} />
           <Route path="/back-office/new-exercice" component={AddExercice} />
           </Switch>
         <Footer />
