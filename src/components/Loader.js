fac997babbf43e046dca234d834a70ec1ac499db
Miura <tadamiura@gmail.com>
2020-12-16 18:56:04 +0100

    writed loader classNames

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Loader = () =>{
     return(
-        <div>
-            Hello !
+        <div className="wrapper">
+            <span className="circle circle-1"></span>
+            <span className="circle circle-2"></span>
+            <span className="circle circle-3"></span>
+            <span className="circle circle-4"></span>
+            <span className="circle circle-5"></span>
+            <span className="circle circle-6"></span>
+            <span className="circle circle-7"></span>
+            <span className="circle circle-8"></span>
         </div>
     )
 }
