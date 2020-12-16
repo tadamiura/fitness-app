30bae469d89238d265597fde5b5b93b80f0dc392
Miura <tadamiura@gmail.com>
2020-12-16 20:04:56 +0100

    renamed backoffice's Dashboard component to BackofficeDashboard

---
+++ src/components/backoffice/BackOfficeDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BackOfficeDashboard = () => {
+    return(
+        <div className='component backoffice-dashboard'>
+            DashBoard
+        </div>
+    )
+}
+
+export default BackOfficeDashboard
