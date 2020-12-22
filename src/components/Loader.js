b52420873193de7b38b7e74b2ceabb398c751a36
Miura <tadamiura@gmail.com>
2020-12-22 19:18:29 +0100

    added a text to loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Loader = () =>{
     return(
         <div className="wrapper">
+            <h2>wicked workout vous prépare quelque chose de beau</h2>
             <span className="circle circle-1"></span>
             <span className="circle circle-2"></span>
             <span className="circle circle-3"></span>
