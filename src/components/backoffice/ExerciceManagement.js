b93e0216894b84a7173a5e124106d29c540eb209
Miura <tadamiura@gmail.com>
2020-12-23 12:22:00 +0100

    added a form to update the info of exercice

--- src/components/backoffice/ExerciceManagement.js
+++ src/components/backoffice/ExerciceManagement.js
@@ -14,6 +14,20 @@ const ExerciceManagement = (props) => {
         .then((res) => setExercice(res.data))
     }
 
+    const handleChange = (e) => {
+        // const { name, value } = e.target
+        setExercice({ ...exercice, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios
+            .put(`/api/exercices/${props.match.params.id}`, exercice)
+            .then((res) => res.data)
+            .then((res) => alert(`${res}, l'exercice a bien été modifié`))
+            .catch((err) => alert(`erreur : ${err.response.data} `))
+    }
+
     const history = useHistory()
 
     const deleteExercice = () =>{
@@ -39,6 +53,34 @@ const ExerciceManagement = (props) => {
                 style={{
                 margin: 'auto'
             }}/>
+
+            <form onSubmit={handleSubmit}>
+                <h1>Modifier l'exercice</h1>
+                <div>
+                    <label htmlFor='name'>Nom de l'exercice</label><br></br>
+                    <input type="text"
+                    name="name"
+                    id="name"
+                    required
+                    value={exercice.name}
+                    onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='url_name'>Lien Url de la vidéo</label><br></br>
+                    <input type="text"
+                    name="url_name"
+                    id="url_name"
+                    required
+                    value={exercice.url_name}
+                    onChange={handleChange}
+                    />
+                </div>
+                <input type='submit' value='Valider'
+                className="default-button"
+                />
+            </form>
+
             <button className="default-button" onClick={deleteExercice}>Supprimer l'exercice</button>
         </div>
     )
