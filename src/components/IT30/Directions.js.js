caa37be3091a55baca505bbbdba3922578e4c05d
Miura <tadamiura@gmail.com>
2021-01-01 17:58:21 +0100

    created directions for it30

---
+++ src/components/IT30/Directions.js.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Directions = () => {
+    return (
+        <div>
+             <h3>Les instructions :</h3>
+                <p>Pendant 8 minutes, alternez deux exercices pendant 30 secondes.</p>
+                <p>A la fin de chaque exercice, prenez une pause de 30 secondes</p>
+                <p>A la fin du circuit, si vous désirez poursuivre votre entraînement, récupérer 2 minutes avant de débuter le prochain circuit</p>
+                <p>Pour une belle séance, 2 à 3 circuits sont optimales</p>
+        </div>
+    )
+}
+
+export default Directions   
