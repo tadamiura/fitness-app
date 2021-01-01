8ce28b093927df14dbae6ddd18f0948ec3793a51
Miura <tadamiura@gmail.com>
2021-01-01 17:59:35 +0100

    created workout and display exercices pages

---
+++ src/components/IT30/DisplayExercice.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import ReactPlayer from 'react-player'
+import Directions from './Directions.js'
+import Loader from '../Loader'
+import '../../App.scss'
+
+const DisplayExercice = ({ ex1, ex2 }) =>{
+
+    const [showCounter, setShowCounter] = useState(false);
+
+
+return( ex1 === null || ex2 === null
+        ?
+        <div className='loader-component'>
+            <Loader />
+        </div>
+        : 
+        <div className='display-exercices'>
+            <div className='random-exercice' id={ex1.id}> 
+                <span className="exercice">{ex1.name}</span>
+                <ReactPlayer 
+                url={ex1.url_name} 
+                controls={true}
+                height={180}
+                width={300}
+                style={{
+                margin: 'auto'
+                }}/>
+            </div>
+            <div className='random-exercice' id={ex2.id}> 
+            <span className="exercice">{ex2.name}</span>
+                <ReactPlayer 
+                url={ex2.url_name} 
+                controls={true}
+                height={180}
+                width={300}
+                style={{
+                margin: 'auto'
+                }}/>
+            </div>
+            <div>
+            <Link to={{
+                pathname: '/it30/countdown',
+                state: {ex1, ex2}
+            }}>
+                <input
+                    className='start-tabata-button'
+                    type='button'
+                    value={!showCounter ? `Lancer le circuit` : `Arrêter le circuit`}
+                    onClick={() => setShowCounter(!showCounter)}
+                />
+            </Link>
+            <Directions />
+            </div>
+        </div>
+        )
+    
+}
+export default DisplayExercice
