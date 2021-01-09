cff6e178038498f25036471abf42dd1c468f31e2
Miura <tadamiura@gmail.com>
2021-01-09 10:43:10 +0100

    improved directions

--- src/components/666 Workout/Directions.js.js
+++ src/components/666 Workout/Directions.js.js
@@ -6,7 +6,11 @@ const Directions = () => {
             <h3>Les instructions :</h3>
             <p>Objectifs : Effectuez le meileur temps possible pour terminer le circuit</p>
             <p>Attention ! La vitesse d'exécution ne doit <span className='bold uppercase'>jamais</span> se faire au détriment de la <span className='bold uppercase'>qualité</span> de l'exécution des exercices</p>
-            <p>Réaliser <span className='bold uppercase'>6 séries</span> de <span className='bold uppercase'>6 répétitions</span> du premier exercice puis <span className='bold uppercase'>6 répétitions</span> du second</p>
+            <p>
+                Déclenchez le chrono au début du circuit<br></br>
+                Arretez-le uniquement à la fin du circuit
+            </p>
+            <p>Réalisez <span className='bold uppercase'>6 séries</span> de <span className='bold uppercase'>6 répétitions</span> du premier exercice puis <span className='bold uppercase'>6 répétitions</span> du second</p>
             <p>Le temps de récupération est <span className='bold uppercase'>libre</span> : vous pouvez prendre une récupération quand vous le voulez pendant le circuit. Vous pouvez prendre autant de temps de récupération que vous le voulez</p>
             <p>Si vous en avez la capacité, vous pouvez effectuer le circuit sans récupération</p>
         </div>
