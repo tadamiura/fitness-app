ecc4ef2f2e1e1e4f64312dc7d972d9e899c545f4
Miura <tadamiura@gmail.com>
2021-01-25 15:46:45 +0100

    improved already user link

--- src/components/authentification/Authentification.js
+++ src/components/authentification/Authentification.js
@@ -27,45 +27,44 @@ function Authentification() {
         <h2>Se connecter</h2>
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="label-input-container">
-              <label >Connectez-vous avec votre adresse mail :</label><br></br>
-              <input 
-              name="email" 
-              ref={register({
-                  required: true,
-                  maxLength: 100,
-                  pattern: /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/,
-              })}             
-              placeholder="Email" 
-              /><br></br>
-              <span className="form-error-message">{errors.email && "Un email valide est requis"}</span>
+                <label >Connectez-vous avec votre adresse mail :</label><br></br>
+                <input 
+                name="email" 
+                ref={register({
+                    required: true,
+                    maxLength: 100,
+                    pattern: /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/,
+                })}             
+                placeholder="Email" 
+                /><br></br>
+              < span className="form-error-message">{errors.email && "Un email valide est requis"}</span>
             </div>
             <div className="label-input-container">
-              <label >Votre mot de passe : </label><br></br>
-              <input 
-              name="password" 
-              ref={register({
-                  required: true,
-                  minLength: 6,
-              })} 
-              placeholder="Password" 
-              type="password"
-              
-              /><br></br>
-              <span className="form-error-message">{errors.password && "Un mot de passe de 6 caractères minimium est requis"}</span>
-
+                <label >Votre mot de passe : </label><br></br>
+                <input 
+                name="password" 
+                ref={register({
+                    required: true,
+                    minLength: 6,
+                })} 
+                placeholder="Password" 
+                type="password"
+                
+                /><br></br>
+                <span className="form-error-message">{errors.password && "Un mot de passe de 6 caractères minimium est requis"}</span>
             </div>
             <div>
               <button type="submit" disabled={formState.isSubmitting}>
                   Valider
               </button>
             </div>
-      </form>
-      <Link
-      to={`/registration`}
-      className='already-get-count'
-      >
-        Pas encore de compte ?
-      </Link>
+            <Link
+            to={`/registration`}
+            className='already-get-count'
+            >
+              Pas encore de compte ?
+            </Link>
+        </form>
     </div>
     )
 }
