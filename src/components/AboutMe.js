9c138aa44d9cce09580ae9a3121fc2e9465f0791
Miura <tadamiura@gmail.com>
2021-01-25 15:17:36 +0100

    fixed link to portfolio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,7 @@ const AboutMe = () => {
         <div className="about">
             <h2>About me</h2>
             <p className="description">Tadatoshi Miura, french strenght and conditioning coach (handball, football, kick-boxing...), fitness coach, sport and health coach and bizness developer</p>
-            <p className="description">Since march 2020, I become web developer. You can visit my portfolio : <a href="www.tadatoshi-miura.com">www.tadatoshi-miura.com</a></p>
+            <p className="description">Since march 2020, I become web developer. You can visit my portfolio : <a href="https://www.tadatoshi-miura.com/">www.tadatoshi-miura.com</a></p>
         </div>
     )
 }
