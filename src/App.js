460fde62fe5f5c3f69f0260d12ba7ea06f7cb12d
Miura <tadamiura@gmail.com>
2021-01-25 14:43:50 +0100

    create a authentification folder to put auth's & register's files, improved routes doc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AboutMe from './components/AboutMe'
-import Authentification from './components/tabata/Authentification'
 import AboutTabata from './components/AboutTabata'
+import Authentification from './components/authentification/Authentification'
 import BackOfficeDashboard from './components/backoffice/BackOfficeDashboard';
 import Countdown from './components/tabata/Countdown'
 import Dashboard from './components/Dashboard'
@@ -12,13 +12,13 @@ import It30Home from './components/IT30/It30Home'
 import It30Countdown from './components/IT30/Countdown'
 import It30TabataWorkout from './components/IT30/Workout'
 import List from './components/backoffice/List'
+import NewExercice from './components/backoffice/NewExercice'
+import Registration from './components/authentification/Registration'
 import SixSixSixHome from './components/666Workout/SixSixSixHome';
 import SixSixSixStopWatch from './components/666Workout/SixSixSixStopWatch';
 import SixSixSixWorkout from './components/666Workout/Workout'
-import NewExercice from './components/backoffice/NewExercice'
 import TabataHome from './components/tabata/TabataHome'
 import TabataWorkout from './components/tabata/Workout'
-import Registration from './components/tabata/Registration'
 import WarmUpList from './components/warmup/WarmUpList'
 import { Switch, Route } from 'react-router-dom'
 
@@ -50,14 +50,18 @@ const App = () => {
           <Route path="/666-workout/workout" component={SixSixSixWorkout}/>
           <Route path="/666-workout/stopwatch" component={SixSixSixStopWatch}/>
 
+          {/* Extra informations */}
           <Route path="/about-me" component={AboutMe} />
           <Route path="/about-tabata" component={AboutTabata} />
+
+          {/* Authentification Routes */}
           <Route path="/registration" component={Registration}/>
           <Route path="/auth/login" component={Authentification}/>
+
+          {/* Back Office's Routes */}
           <Route exact path="/back-office" component={BackOfficeDashboard} />
           <Route path="/back-office/exercices" component={List} />
           <Route path="/back-office/exercice/:id" render={(props) => <ExerciceManagement {...props} />}/>
-          {/* <Route path="/back-office/new-exercice" component={AddExercice} /> */}
           <Route path="/back-office/new-exercice" component={NewExercice} />
           </Switch>
         <Footer />
